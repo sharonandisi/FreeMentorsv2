768e5ac9791a825532516443985824aebbec0413
sharonandisi <sharonandisi.28@gmail.com>
2019-09-10 15:10:53 +0300

    [#168361720]write the createadmin middleware function meant to be added to the sign in route

---
+++ server/middleware/admin.js
@@ -0,0 +1,24 @@
+import userModel from '../models/userModel';
+import authHelper from '../helpers/authHelper';
+import '../../config';
+
+const createAdmin = (req, res, next) => {
+    const user = userModel.findAdmin();
+    const hashpassword = authHelper.hashPassword(process.env.ADMIN_PASSWORD);
+    if (!user) {
+        userModel.createAdmin({
+            firstname: 'admin',
+            lastname: 'admin',
+            email: process.env.ADMIN_EMAIL,
+            password: hashpassword,
+            address: 'Nairobi',
+            occupation: 'Admin',
+            expertise: 'admin',
+            bio: 'I am an admin',
+            isAdmin: true,
+        });
+    }
+    next();
+};
+
+export default createAdmin;
