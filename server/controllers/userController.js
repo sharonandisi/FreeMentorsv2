4369aba0f3cf2d46ddc51709df99fc0b1cf5d5a5
sharonandisi <sharonandisi.28@gmail.com>
2019-09-11 19:15:56 +0300

    [#168434512]update the controller to use the message helpers, have the right imports from models, and to use the response helper

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,72 +1,50 @@
 import authHelper from '../helpers/authHelper';
-import userModel from '../models/userModel';
-import db from "../db";
+import { create, findByEmail, changeMentor, } from '../models/userModel';
+import messageHelper from '../helpers/messageHepler';
+import response from '../helpers/responseHelper';
 
-class user {
-    /**
-     * @param {object} req
-     * @param {object} res
-     * @returns {object} user object
-     */
 
-     static async create(req, res) {
-         const {
-             firstname, lastname, email, password, address, occupation, bio, expertise,} = req.body;
 
-        try {
-         const hashpassword = authHelper.hashPassword(req.body.password.trim());
-         const rows = await userModel.create({
-             firstname: firstname.trim(),
-             lastname: lastname.trim(),
-             email: email.trim(),
-             password: hashpassword,
-             address: address.trim(),
-             occupation: occupation.trim(),
-             bio: bio.trim(),
-             expertise:expertise.trim(),
-         });
-         const token = authHelper.generateToken(rows[0]);
-         return res.status(201).json({
-             status: 201,
-             message: 'successful',
-             data: ({
-                 token
-             }),
-         });
-        } catch(error) {
-            if(error.routine === '_bt_check_unique'){
-                return res.status(409).json({
-                    status: 409,
-                    error: "Email already in use"
-                })
-            }
-            return res.status(400).json({
-                status: 400,
-                error: "Bad request"
-
-            })
+async function Create(req, res) {
+    const { body } = req;
+    try {
+        const hashpassword = authHelper.hashPassword(body.password);
+        const rows = await create(body, hashpassword);
+        console.log(rows)
+        const token = authHelper.generateToken(rows);
+        return response(res, 201, messageHelper.users.auth.userSignup, token)
+    } catch (error) {
+        if (error.routine === '_bt_check_unique') {
+            return response(res, 400, messageHelper.users.auth.emailUnavailable)
         }
+        return response(res, 500, messageHelper.users.failed.catchError)
 
-}
-    static async login(req,res) {
-    const text = 'SELECT * FROM users WHERE email = $1';
-    try{
-        const { rows } = await db.query(text, [req.body.email]);
-        const token = authHelper.generateToken({id:rows[0].id, email:rows[0].email});
-        return res.status(200).json({
-            status: 200,
-            message: 'Successfully logged in',
-            data: ({
-                token
-            })
-        });
-    }catch(error) {
-        return res.status(500).json({
-            status: 500,
-            error: 'Internal server error'
-        })
     }
 }
+
+async function login(req, res) {
+    const { email } = req.body;
+    try {
+        const rows = await findByEmail(email);
+        const token = authHelper.generateToken(rows);
+        return response(res, 200, messageHelper.users.auth.userSignin, token)
+    } catch (error) {
+        return response(res, 400, messageHelper.users.failed.failed)
+    }
 }
 
-export default user;
+async function ChangeMentor(req, res) {
+    try {
+        const rows = await changeMentor(req.params.id);
+        console.log(rows)
+        const { mentorstatus } = rows;
+        return response(res, 200, messageHelper.users.changeMentor, { mentorstatus })
+    } catch (error) {
+        return response(res, 500, messageHelper.users.failed.catchError)
+    }
+}
+module.exports = {
+    Create,
+    login,
+    ChangeMentor
+}
