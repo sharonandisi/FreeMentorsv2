0e578eef59c361350e0be49873f4302e0874be38
sharonandisi <sharonandisi.28@gmail.com>
2019-09-11 23:13:22 +0300

    [#168361720]update the user routes by adding the patch route and the create an admin middlware function to the sign in route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,9 +2,11 @@ import express from 'express';
 import { Create, login } from '../controllers/userController';
 import Validations from '../middleware/userValidation';
 import Verify from '../middleware/userVerification';
+import { CreateAdmin } from '../middleware/admin'
 
 const router = express.Router();
 
 router.post('/signup', Validations.validateSignup, Create);
-router.post('/signin', Validations.validateLogin, Verify.verifyRegistereduser, Verify.verifyPassword, login);
+router.post('/signin', Validations.validateLogin, Verify.verifyRegistereduser, Verify.verifyPassword, CreateAdmin, login);
+router.patch('/:id', Verify.verifyauthenUser, Verify.verifyAdmin, ChangeMentor);
 export default router;
