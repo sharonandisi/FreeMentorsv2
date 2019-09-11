62040f9443ede02d4f9ffa47a9e6e3475f07f630
sharonandisi <sharonandisi.28@gmail.com>
2019-09-11 19:13:38 +0300

    [#168434512]create a messages helper array that stores all the messages needed for errors and success responses throughout the app

---
+++ server/helpers/messageHelper.js
@@ -0,0 +1,39 @@
+const messages = {
+    users: {
+        auth: {
+            emailUnavailable: 'This email is already in use',
+            userSignup: 'User has been successfully signed up',
+            userSignin: 'User has been successfully signed in',
+            changeMentor: 'User has successfully be changed to a mentor',
+            notUSer: 'User does not exist',
+            invalidPassword: 'Please enter a valid password',
+            tokenFailure: 'Failed to fetch token.Please try again',
+            access: 'Access denied',
+            mentorStatus: 'Mentor status not activated'
+        },
+
+        mentors: {
+            mentor: 'Success. Mentor found',
+            mentors: 'Success. All mentors found',
+            noMentor: 'No mentors have been found'
+        },
+
+        failed: {
+            failed: 'Bad request',
+            catchError: 'Internal sever error',
+            signinFail: 'Please sign up to access this service'
+        }
+    },
+    sessions: {
+        sessions: {
+            session: 'Session successfully created',
+            notSession: 'Session does not exist',
+            acceptConflict: 'Conflict. Session already accepted',
+            rejectConflict: 'Conflict.Session already rejected',
+            accept: 'Session request accepted',
+            decline: 'Session request rejected'
+        }
+    }
+}
+
+module.exports = messages;
