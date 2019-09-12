ea2d9b7ce9821dd3edac881e4264dd2a6f5220ee
sharonandisi <sharonandisi.28@gmail.com>
2019-09-13 00:40:10 +0300

    fix params validation bug

--- server/middleware/paramsIntVerification.js
+++ server/middleware/paramsIntVerification.js
@@ -4,7 +4,7 @@ import messageHelper from '../helpers/messageHelper';
 
 export default async (req, res, next) => {
     const param = req.params.id;
-    if (parseInt(param) < 1 || parseInt(param) > 1000 || typeof(param) !== 'number') 
+    if (parseInt(param) < 1 || parseInt(param) > 1000 || typeof(param) === 'number') 
         return responseHelpler(res, 400, messageHelper.users.failed.failed)
     next();
 }
