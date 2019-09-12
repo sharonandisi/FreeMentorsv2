4e6a6be4953833f4b6dd74f105d3ae808e257c3c
sharonandisi <sharonandisi.28@gmail.com>
2019-09-12 05:59:44 +0300

    [#Delivers 168361720]ensure that all tests are passing

--- server/middleware/userVerification.js
+++ server/middleware/userVerification.js
@@ -1,35 +1,32 @@
 import jwt from 'jsonwebtoken';
-import userModel from '../models/userModel';
+import { findByEmail } from '../models/userModel';
 import authHelper from '../helpers/authHelper';
-import db from "../db";
 import '../../config';
+import messageHelper from '../helpers/messageHelper';
+import response from '../helpers/responseHelper';
 
-class Verify {
-    /**
-     * class constructor
-     * @param { object } data
-     */
 
-    async verifyUser(req, res, next) {
+
+    async function verifyUser(req, res, next) {
         const user = await findByEmail(req.body.email);
         if (user) return response(res, 400, messageHelper.users.auth.emailUnavailable)
         return next();
     }
 
-    async verifyRegistereduser(req, res, next) {
+    async function verifyRegistereduser(req, res, next) {
         const user = await findByEmail(req.body.email);
         if (!user) return response(res, 400, messageHelper.users.failed.signinFail)
         return next();
     }
 
-    async verifyexistingUser(req, res, next) {
+    async function  verifyexistingUser(req, res, next) {
         const { userid } = req.decoded;
         const user = await userModel.findOne(userid);
         if (!user) return response(res, 400, messageHelper.users.auth.notUSer)
         return next();
     }
 
-    async verifyPassword(req, res, next) {
+    async function verifyPassword(req, res, next) {
         const user = await findByEmail(req.body.email);
         if (!authHelper.comparePassword(user.password, req.body.password)) {
             return response(res, 401, messageHelper.users.auth.invalidPassword)
@@ -37,7 +34,7 @@ class Verify {
         return next();
     }
 
-    verifyauthenUser(req, res, next) {
+    async function verifyauthenUser(req, res, next) {
         const token = req.header('x-auth-token');
         if (!token) {
             return response(res, 401, messageHelper.users.auth.tokenFailure)
@@ -51,14 +48,14 @@ class Verify {
         return next();
     }
 
-    async verifyAdmin(req, res, next) {
+    async function verifyAdmin(req, res, next) {
         const { email } = req.decoded;
         const user = await findByEmail(email);
         if (!user.isadmin) return response(res, 403, messageHelper.users.auth.access)
         return next();
     }
 
-    async checkmentorStatus(req, res, next) {
+    async function checkmentorStatus(req, res, next) {
         const { mentorid } = req.body;
         const mentor = await userModel.findOne(mentorid);
         if (!mentor || mentor.mentorstatus !== 'true') {
@@ -67,7 +64,7 @@ class Verify {
         return next();
     }
 
-    async verifyMentor(req, res, next) {
+    async function verifyMentor(req, res, next) {
         const { mentorid } = req.decoded.payload;
         const mentor = await userModel.findOne(mentorid);
         if (!mentor || User.mentorstatus !== 'true') {
@@ -75,6 +72,13 @@ class Verify {
         }
         return next()
     }
+module.exports = {
+    verifyUser,
+    verifyRegistereduser,
+    verifyexistingUser,
+    verifyPassword,
+    verifyauthenUser,
+    verifyAdmin,
+    checkmentorStatus,
+    verifyMentor
 }
-
-export default new Verify();
--- server/tests/auserTest.js
+++ server/tests/auserTest.js
@@ -1,9 +1,10 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../../server';
-import User from '../models/userModel';
+import { findByEmail } from '../models/userModel';
 import '../../config';
 import testdata from './mockData/user';
+import authHelper from '../helpers/authHelper';
 
 const { expect } = chai;
 chai.should();
@@ -11,6 +12,19 @@ chai.use(chaiHttp);
 
 describe('/Auth', () => {
     describe('/POST signup', () => {
+        it('should successfully sign up a user', (done) => {
+            const { user001 } = testdata;
+            chai.request(app)
+                .post('/api/v1/auth/signup')
+                .send(user001)
+                .end((err, res) => {
+                    res.should.have.status(201);
+                    if (err) return done();
+                    done();
+                });
+        });
+
+
         it('should not sign up a user missing a firstname', (done) => {
             chai.request(app)
                 .post('/api/v1/auth/signup')
@@ -102,6 +116,18 @@ describe('/Auth', () => {
     });
 
     describe('/POST signin', () => {
+        it('should successfully sign in a user', (done) => {
+            const { user010 } = testdata;
+            chai.request(app)
+                .post('/api/v1/auth/signin')
+                .send(user010)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    if (err) return done();
+                    done();
+                });
+        });
+
         it('should not sign in a user missing an email address', (done) => {
             chai.request(app)
                 .post('/api/v1/auth/signin')
@@ -133,7 +159,53 @@ describe('/Auth', () => {
                     if (err) return done();
                     done()
                 })
-        })
+        }) 
     });
+    describe('PATCH /api/v1/auth/:userid', () => {
+        let id = '';
+        let token = '';
 
+        const execute = () => chai.request(app)
+            .patch(`/api/v1/auth/${id}`)
+            .set('x-auth-token', token);
+
+        it('should not allow an admin access with no token', async () => {
+            const admin = await findByEmail(process.env.ADMIN_EMAIL);
+            id = admin.id;
+            token = '';
+            const res = await execute();
+            expect(res).to.have.status(401);
+        });
+
+        it('should not allow an admin access with invalid token', async () => {
+            const admin = await findByEmail(process.env.ADMIN_EMAIL);
+            id = admin.id;
+            token = 'jdje';
+            const res = await execute();
+            expect(res).to.have.status(401);
+        });
+
+
+        it('should check for admin before allowing change of status', async () => {
+
+            const { user001 } = testdata;
+            const user = await findByEmail(user001.email);
+            id = user.id;
+            token = authHelper.generateToken(user);
+            const res = await execute();
+            expect(res).to.have.status(403);
+        });
+
+        it('should allow an admin to change a status', async () => {
+            const { user001 } = testdata;
+            const user = await findByEmail(user001.email);
+            id = user.id
+            const admin = await findByEmail(process.env.ADMIN_EMAIL);
+            token = authHelper.generateToken(admin);
+            const res = await execute();
+            expect(res).to.have.status(200);
+
+        });
+
+    })
 });
