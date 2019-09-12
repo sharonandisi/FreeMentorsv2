67caaf8c627fd089e434823967f523697056c1cd
sharonandisi <sharonandisi.28@gmail.com>
2019-09-12 10:27:55 +0300

    [#Delivers 168361731]add fetch a specific mentor tests

--- server/routes/mentorRoutes.js
+++ server/routes/mentorRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { fetchSpecificMentor } from '../controllers/mentorController';
-import {verifyauthenUser} from '../middleware/userVerification';
+import { verifyauthenUser} from '../middleware/userVerification';
 
 const router = express.Router();
 
